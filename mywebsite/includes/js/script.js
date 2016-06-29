83f02bd391b0090a7b3df9cfb6e7ec2aebcb2bf3
Attish <Attish.Raj@cognizant.com>
2016-06-29 19:04:59 +0530

    mid video22

--- mywebsite/includes/js/script.js
+++ mywebsite/includes/js/script.js
@@ -14,5 +14,10 @@ $(function ()
     e.preventDefault();
     $('#successAlert').slideDown();
   });
+  $('a.pop').click(function(e) {
+    e.preventDefault();
+  });
+  $('a.pop').popover();
+  $('[rel="tooltip"]').tooltip();
 }
 );
