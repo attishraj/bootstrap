863fa35a7bc4c368a2336a7f636e7ddc79abd19d
attishraj <Attish.Raj@cognizant.com>
2016-07-01 08:47:22 +0530

    night study

--- assg1/includes/js/script.js
+++ assg1/includes/js/script.js
@@ -11,3 +11,38 @@ Notes:	 Hand coded for Udemy.com
 $('.carousel').carousel({
   interval: 2500
 })
+
+.cssanimations .cd-timeline-img.is-hidden {
+  visibility: hidden;
+}
+.cssanimations .cd-timeline-img.bounce-in {
+  visibility: visible;
+  animation: cd-bounce-1 0.6s;
+}
+ 
+@keyframes cd-bounce-1 {
+  0% {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+ 
+  60% {
+    opacity: 1;
+    transform: scale(1.2);
+  }
+ 
+  100% {
+    transform: scale(1);
+  }
+}
+
+
+$(window).on('scroll', function(){
+	$timeline_block.each(function(){
+		if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+			$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		}
+	});
+});
+C
+
